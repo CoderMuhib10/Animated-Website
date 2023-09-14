536ec1848c20724e04f50c7264bd5233ce45c8fd
CoderMuhib10 <kidbecomingcreative12@gmail.com>
2023-09-14 14:12:44 +0600

    added the about us section

--- script.js
+++ script.js
@@ -1,33 +1,37 @@
-var cursor= document.querySelector("#cursor")
-var cursorBlur= document.querySelector("#cursor-blur")
-document.addEventListener("mousemove",function (dets) {
-    cursor.style.left=dets.x+"px"
-    cursor.style.top =dets.y+"px" 
-    cursorBlur.style.left=dets.x- 250 +"px"
-    cursorBlur.style.top =dets.y- 250 +"px" 
-})
+// custom cursor code starts
+var cursor = document.querySelector("#cursor");
+var cursorBlur = document.querySelector("#cursor-blur");
+document.addEventListener("mousemove", function (dets) {
+  cursor.style.left = dets.x + "px";
+  cursor.style.top = dets.y + "px";
+  cursorBlur.style.left = dets.x - 250 + "px";
+  cursorBlur.style.top = dets.y - 250 + "px";
+});
 
-gsap.to("#nav",{
-    backgroundColor:"#000",
-    duration:0.5,
-    height:"110px",
-    scrollTrigger:{
-        trigger:"#nav",
-        scroller:"body",
-        // markers:true,
-        start:"top -10%",
-        end:"top -11%",
-        scrub:1
-    }
-})
-gsap.to("#main",{
-    backgroundColor:"#000",
-    scrollTrigger:{
-        trigger:"#main",
-        scroller:"body",
-        // markers:true,
-        start:"top -25%",
-        end:"top -70%",
-        scrub:2
-    }
-})
+// on scroll navbar color changing gsap code starts
+
+gsap.to("#nav", {
+  backgroundColor: "#000",
+  duration: 0.5,
+  height: "110px",
+  scrollTrigger: {
+    trigger: "#nav",
+    scroller: "body",
+    // markers:true,
+    start: "top -10%",
+    end: "top -11%",
+    scrub: 1,
+  },
+});
+// on scroll background color changing gsap code starts
+gsap.to("#main", {
+  backgroundColor: "#000",
+  scrollTrigger: {
+    trigger: "#main",
+    scroller: "body",
+    // markers:true,
+    start: "top -25%",
+    end: "top -70%",
+    scrub: 2,
+  },
+});
