12942a2d80bb11d391c95a29b7c27b4b73d1e672
CoderMuhib10 <kidbecomingcreative12@gmail.com>
2023-09-14 15:48:57 +0600

    added the card section

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 var cursor = document.querySelector("#cursor");
 var cursorBlur = document.querySelector("#cursor-blur");
 document.addEventListener("mousemove", function (dets) {
-  cursor.style.left = dets.x + "px";
+  cursor.style.left = dets.x + 25 + "px";
   cursor.style.top = dets.y + "px";
   cursorBlur.style.left = dets.x - 250 + "px";
   cursorBlur.style.top = dets.y - 250 + "px";
@@ -33,5 +33,62 @@ gsap.to("#main", {
     start: "top -25%",
     end: "top -70%",
     scrub: 2,
+  },
+});
+
+// cursor change on hover
+var h4all = document.querySelectorAll("#nav h4");
+h4all.forEach(function (elem) {
+  elem.addEventListener("mouseenter", function () {
+    cursor.style.scale = 3;
+    cursor.style.outline = "1px solid #fff";
+    cursor.style.backgroundColor = "transparent";
+  });
+  elem.addEventListener("mouseleave", function () {
+    cursor.style.scale = 1;
+    cursor.style.outline = "0px solid #95C11E";
+    cursor.style.backgroundColor = "#95C11E";
+  });
+});
+
+gsap.to("#nav", {
+  backgroundColor: "#000",
+  duration: 0.5,
+  height: "110px",
+  scrollTrigger: {
+    trigger: "#nav",
+    scroller: "body",
+    // markers:true,
+    start: "top -10%",
+    end: "top -11%",
+    scrub: 1,
+  },
+});
+gsap.from("#about-us img,#about-us-in", {
+  y: 90,
+  opacity: 0,
+  duration: 1,
+  satagger: 0.4,
+  scrollTrigger: {
+    trigger: "#about-us",
+    scroller: "body",
+    // markers: true,
+    start: "top 60%",
+    end: "top 58%",
+    scrub: 3,
+  },
+});
+gsap.from(".card", {
+  scale: 0.8,
+  opacity: 0,
+  duration: 1,
+  satagger: 0.1,
+  scrollTrigger: {
+    trigger: ".card",
+    scroller: "body",
+    // markers: true,
+    start: "top 70%",
+    end: "top 65%",
+    scrub: 3,
   },
 });
